3edf599708587988adebf62b6fe3f30ce400decb
Illyasviel <785291773@qq.com>
2021-10-25 22:51:58 +0800

    sth

--- exp5/password.cpp
+++ exp5/password.cpp
@@ -8,14 +8,14 @@ string *g;
 
 int e;
 
-void ts() {
+void ts(string& str) {
     char c;
-    g[0] = s;
+    g[0] = str;
     for (int i = 1; i<e ; i++) {
-        c = s[0];
-        s.erase(s.begin());
-        s.push_back(c);
-        g[i] = s;
+        c = str[0];
+        str.erase(str.begin());
+        str.push_back(c);
+        g[i] = str;
     }
 }
 
@@ -23,17 +23,21 @@ int findidx(const string &str, char c) {
     for (int i = int(str.size()) - 1; i >= 0; i--) {
         if (str[i]==c) return i;
     }
+    return -1;
 }
 
-string* scan(const string& str, int* idx) {
-    char c = s[0];
-
-    for (int i = 0; i < s.size(); i++) {
-        for (int j = 0; j < str.size(); j++) {
-            if ( str[j]==s[i] ) c = str[j];
-
-        }
-    }
+string scan(const string& str) {
+    string ans;
+    ans+=s[0];
+    char c = str[0];
+    ans+=c;
+    int i;
+    do {
+        i = findidx(s, c);
+        c = str[i];
+        ans+=c;
+    } while(ans.size()<e);
+    return ans;
 }
 
 int main() {
@@ -45,24 +49,29 @@ int main() {
             cin >> s;
             e = int(s.length());
             g = new string[s.size()];
-            ts();
+            ts(s);
             sort(&g[0], &g[0]+s.size(), [&](const string &a, const string &b) {
                 return a[0]<b[0];
             });
             string ans;
             for (int i = 0; i < e; ++i) {
                 ans += *g[i].rbegin();
-                //        cout << g[i] << "\n";
+//                cout << g[i] << "\n";
             }
             printf("encrypted: %s.\n", ans.c_str());
         }
         else {
             cin >> s;
+            e = int(s.size());
             string ms = s;
             sort(ms.begin(), ms.end());
 //            cout << ms;
-            int* idx = new int[s.size()]{0};
-            scan(ms, idx);
+            auto ans = scan(ms);
+            ts(ans);
+            printf("Possible solution:\n");
+            for (int i = 0; i<e ; ++i) {
+                cout << g[i] << "\n";
+            }
         }
     }
 
